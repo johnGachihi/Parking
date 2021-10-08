4391393d6421b83b1517aa2327cd074c6db6ac8a
johnGachihi <johngachihi3@gmail.com>
2021-10-08 16:22:45 +0300

    [WIP] Add auth

--- src/main/kotlin/com/johngachihi/parking/web/security/Configuration.kt
+++ src/main/kotlin/com/johngachihi/parking/web/security/Configuration.kt
@@ -1,6 +1,7 @@
 package com.johngachihi.parking.web.security
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -9,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 open class Configuration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll()
+        http.cors(withDefaults()).csrf().disable().authorizeRequests().anyRequest().permitAll()
+            .and()
+            .formLogin()
+            .successHandler { _, _, _ ->  }
     }
 }
